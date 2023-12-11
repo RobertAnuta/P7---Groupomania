59b05f28e71ee964ca126e5c82ce6103633e72b5
Robert <118054116+RobertAnuta@users.noreply.github.com>
2023-12-10 22:03:09 -0800

    Update connect.js

--- api/connect.js
+++ api/connect.js
@@ -3,6 +3,6 @@ import mysql from 'mysql'
 export const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'R@bert1991',
+  password: 'db_password",
   database: 'db_groupomania'
 })
